b60be8a0ba32b38b3a49d47c58339f6012a2ee4d
SaranyaPandiaraj <52775239+SaranyaPandiaraj@users.noreply.github.com>
2019-08-27 01:03:43 -0700

    Fourth Version

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,16 +3,15 @@ import csv
 
 # Path to collect data from the Resources folder
 budget_csv = os.path.join('..', 'PyBank','Resources', 'budget_data.csv')
-print(budget_csv)
+#print(budget_csv)
 
-Months_Count = []
+Total_Months = []
 Net_Total_Amount = []
 Change_Profit_Loss = []
 
 # Function to return the average
 def average(Change_Profit_Loss): 
     return round(sum(Change_Profit_Loss) / len(Change_Profit_Loss),2) 
-
 
 
 # Read in the CSV file
@@ -22,27 +21,34 @@ with open(budget_csv, 'r') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
             
     csv_header = next(csvreader)
-    print(f"CSV Header: {csv_header}")
+    #print(f"CSV Header: {csv_header}")
     
     # Using Comprehension Method :
-    #Months_Count = [Month for Month in csvreader]
-    #print(len(Months_Count))
+    #Total_Months = [Month for Month in csvreader]
+    #print(len(Total_Months))
     
     # Loop through the data
     for row in csvreader:
         
-        Months_Count = Months_Count + [row[0]]
+        Total_Months = Total_Months + [row[0]]
         Net_Total_Amount.append(int(row[1]))
         
     for i in range(len(Net_Total_Amount)-1):
-        Change_Profit_Loss.append(Net_Total_Amount[i+1]-Net_Total_Amount[i])   
+	
+        Change_Profit_Loss.append(Net_Total_Amount[i+1] - Net_Total_Amount[i])   
 
 Greatest_Increase = max(Change_Profit_Loss)
-Greatest_Decrease = min(Change_Profit_Loss)		
+Greatest_Decrease = min(Change_Profit_Loss)	
+
+Greatest_Increase_Month = Total_Months[Change_Profit_Loss.index(Greatest_Increase)+1]
+Greatest_Decrease_Month = Total_Months[Change_Profit_Loss.index(Greatest_Decrease)+1]
         
-        
-print(f"Total Months : {len(Months_Count)}")
-print(f"Total : ${sum(Net_Total_Amount)}")
-print(f"Average Change: ${average(Change_Profit_Loss)}")
-print(f" $ {str(Greatest_Increase)}")
-print(f" $ {str(Greatest_Decrease)}")
+print("-----------------------------------------------------")
+print("		Financial Analysis		")    
+print("-----------------------------------------------------\n")    
+print(f" Total Months : {len(Total_Months)}\n")
+print(f" Total : ${sum(Net_Total_Amount)}\n")
+print(f" Average Change: ${average(Change_Profit_Loss)}\n")
+print(f" Greatest Increase in Profits: {Greatest_Increase_Month} (${str(Greatest_Increase)})\n")
+print(f" Greatest Decrease in Profits: {Greatest_Decrease_Month} (${str(Greatest_Decrease)})\n")
+print("-----------------------------------------------------")  
