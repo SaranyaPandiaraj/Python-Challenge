f1062b5c0000d31e308ea62ddded935b597b4d23
SaranyaPandiaraj <52775239+SaranyaPandiaraj@users.noreply.github.com>
2019-08-27 00:35:36 -0700

    Third Version

--- PyBank/main.py
+++ PyBank/main.py
@@ -5,18 +5,44 @@ import csv
 budget_csv = os.path.join('..', 'PyBank','Resources', 'budget_data.csv')
 print(budget_csv)
 
+Months_Count = []
+Net_Total_Amount = []
+Change_Profit_Loss = []
+
+# Function to return the average
+def average(Change_Profit_Loss): 
+    return round(sum(Change_Profit_Loss) / len(Change_Profit_Loss),2) 
+
+
+
 # Read in the CSV file
 with open(budget_csv, 'r') as csvfile:
 
     # Split the data on commas
     csvreader = csv.reader(csvfile, delimiter=',')
-			
+            
     csv_header = next(csvreader)
     print(f"CSV Header: {csv_header}")
-	
+    
+    # Using Comprehension Method :
+    #Months_Count = [Month for Month in csvreader]
+    #print(len(Months_Count))
+    
     # Loop through the data
     for row in csvreader:
-		
-        #if row[0] == 'Jan-2010':
-		
-            print(row[0]))
+        
+        Months_Count = Months_Count + [row[0]]
+        Net_Total_Amount.append(int(row[1]))
+        
+    for i in range(len(Net_Total_Amount)-1):
+        Change_Profit_Loss.append(Net_Total_Amount[i+1]-Net_Total_Amount[i])   
+
+Greatest_Increase = max(Change_Profit_Loss)
+Greatest_Decrease = min(Change_Profit_Loss)		
+        
+        
+print(f"Total Months : {len(Months_Count)}")
+print(f"Total : ${sum(Net_Total_Amount)}")
+print(f"Average Change: ${average(Change_Profit_Loss)}")
+print(f" $ {str(Greatest_Increase)}")
+print(f" $ {str(Greatest_Decrease)}")
